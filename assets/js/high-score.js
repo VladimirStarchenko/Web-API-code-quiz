f2b552a7b2db64522336407f75e3a430c0f82ce4
Vladimir Starchenko <vladimistar@gmail.com>
2021-11-14 21:45:37 -0500

    files deleted

--- assets/js/high-score.js
+++
@@ -1,8 +0,0 @@
-const highScoreList = document.querySelector("#highscores");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-highScoreList.innerHTML = highScores
-  .map((score) => {
-    return `<li class="high-score>${initial.initial} - ${score.timeRemaining}</li>`;
-  })
-  .join("");
